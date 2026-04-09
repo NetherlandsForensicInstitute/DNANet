f3f1982ee8bc852bacb14bed26dea1bdefcdd38b
Abel de Wit <abeldewit@gmail.com>
2026-04-09 08:08:00 +0200

    Fixed formatting

--- src/dnanet/data/strategies/scaling/powerplex_y23.py
+++ src/dnanet/data/strategies/scaling/powerplex_y23.py
@@ -11,28 +11,10 @@ class PowerplexY23(PowerPlexFusion6CStrategy):
 
     def marker_name_to_dye_idx(self) -> dict[str, int]:
         return {
-            'DYS576': 0,
-            'DYS389I': 0,
-            'DYS448': 0,
-            'DYS389II': 0,
-            'DYS19': 0,
-            'DYS391': 1,
-            'DYS481': 1,
-            'DYS549': 1,
-            'DYS533': 1,
-            'DYS438': 1,
-            'DYS437': 1,
-            'DYS570': 2,
-            'DYS635': 2,
-            'DYS390': 2,
-            'DYS439': 2,
-            'DYS392': 2,
-            'DYS643': 2,
-            'DYS393': 3,
-            'DYS458': 3,
-            'DYS385': 3,
-            'DYS456': 3,
-            'YGATAH4': 3,
+            'DYS576': 0, 'DYS389I': 0, 'DYS448': 0, 'DYS389II': 0, 'DYS19': 0,
+            'DYS391': 1, 'DYS481': 1, 'DYS549': 1, 'DYS533': 1, 'DYS438': 1, 'DYS437': 1,
+            'DYS570': 2, 'DYS635': 2, 'DYS390': 2, 'DYS439': 2, 'DYS392': 2, 'DYS643': 2,
+            'DYS393': 3, 'DYS458': 3, 'DYS385': 3, 'DYS456': 3, 'YGATAH4': 3,
         }
 
     def dye_channel_colors(self) -> list[str]:
